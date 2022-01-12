1b8bb5e1d104edf10b4148aed5cfd1fc6c4bd065
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-01-12 19:37:31 +0530

    code added

---
+++ scriptAndPages/about.js
@@ -0,0 +1,35 @@
+let about = document.getElementById("about")
+about.innerHTML = `
+<section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/Gireeshkumar.png" alt=""
+                        style="border-radius: 300px; background-color: rgba(146, 139, 139, 0.068);">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Gireeshkumar and I'm a <span class="typing-2"></span></div>
+                    <p>Aspiring Full StackWeb Developer, a passionate coder, and capable of writing efficient code using
+                        MERN Stack. Ambitious to work and collaborate with other dedicated and enterprising developers.
+                        <br>
+                        Eager to take up a responsible role within an exciting and inspiring organization, and deliver
+                        products that offer a great user experience.
+                    </p>
+                    <div class="mediaLinksAbout">
+                        <a href="https://www.linkedin.com/in/gireeshkumar-ediga-4576521a5/" target="_blank">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                        <a href="https://github.com/gireeshkumarEdiga" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </div>
+                    <p class="iconsAboutSection"> <i class="fas fa-phone"></i> +91-7997432307</p>
+                    <p class="iconsAboutSection"><i class="fas fa-envelope"></i> edigagireeshkumar@gmail.com</p>
+                    <a href="https://drive.google.com/file/d/1qZoaFayVNHqSThxQ-1TZpUcD6F1GaBni/view?usp=sharing"
+                        target="_blank" class="resumeToDrive">Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
---
+++ scriptAndPages/projects.js
@@ -0,0 +1,109 @@
+let projectsSection = document.getElementById("mainDivProjectsSection");
+projectsSection.innerHTML = `
+            <!-- product hunt -->
+            <div class="eachProjectDiv MailChimp">
+                <p class="ProjectName ">Google Digital Garage</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/pic02.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                    <p>Developed a clone of <a href="https://learndigital.withgoogle.com/digitalgarage/"
+                    style="color: #FFF; text-decoration: underline;" target="_blank">Google digital Garage</a> website<p >
+                        <br >
+                        Along with 2 other members, build the application in 6 days
+                        </p>
+                        <br>
+                        <p>The Digital Garage is a non-profit nationwide program from Google delivering free digital skills training via an online learning platform. This platform provides individuals with a tailored training plan to learn digital skills, completely for free./p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                        <a href="https://github.com/mausam-kumar/googleDigitalGarage" target="_blank"><button>
+                        Github</button></a>
+                        <a href="https://gireeshkumarediga.github.io/googleDigitalGarage2/HTML/main.html" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!-- mailchimp project -->
+            <div class="eachProjectDiv ">
+                <p class="ProjectName ">Dermstore</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/pic03.png" alt="" />
+                    </div>
+                     <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://www.tanishq.co.in//"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">Tanishq</a> website</p>
+                        <br >
+                        <p> Along withy 5 other members, build lybrate clone in 6 days</p>
+                        </p>
+                        <br>
+                        <p>Tanishq has emerged as India’s fastest growing jewellery brand and is a name which signifies superior craftsmanship, exclusive designs and superlative product quality.</p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/gireeshkumarEdiga/Dermstore"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://gireeshkumarediga.github.io/Dermstore/HTML/main.html" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- lybrate project -->
+            <div class="eachProjectDiv">
+                <p class="ProjectName">Tanishq</p>
+                <div class="ProjectContainer">
+                    <div class="ProjectImage">
+                        <img src="./images/pic04.png" alt="" />
+                    </div>
+                    <div class="projectDescription">
+                        <p>Developed a clone of <a href="https://www.tanishq.co.in//"
+                                style="color: #FFF; text-decoration: underline;" target="_blank">Tanishq</a> website</p>
+                        <br >
+                        <p> Along withy 5 other members, build lybrate clone in 6 days</p>
+                        </p>
+                        <br>
+                        <p>Tanishq has emerged as India’s fastest growing jewellery brand and is a name which signifies superior craftsmanship, exclusive designs and superlative product quality.</p>
+                        <br>
+                        <u style="text-decoration: underline;">TechStack :- </u>
+                        <br>
+                        <div class="techStackProject">
+                            <p><i class="fab fa-html5"></i> HTML</p>
+                            <p><i class="fab fa-css3-alt"></i> CSS</p>
+                            <p> <i class="fab fa-js-square"></i> JS </p>
+                            
+                        </div>
+                        <div class="ProjectLinks">
+                            <a href="https://github.com/Upsehu/Tanishq"
+                                target="_blank"><button>
+                                    Github</button></a>
+                            <a href="https://gireeshkumarediga.github.io/Tanishq/FINAL_OVERVIEW/TANISHQ_FINAL/" target="_blank"><button>Demo</button></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+`;
