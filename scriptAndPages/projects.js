9928c0125ee13967c0b9cb51f916f952cbfa2855
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2022-01-13 11:32:22 +0530

    code added

--- scriptAndPages/projects.js
+++ scriptAndPages/projects.js
@@ -14,7 +14,7 @@ projectsSection.innerHTML = `
                         Along with 2 other members, build the application in 6 days
                         </p>
                         <br>
-                        <p>I developed certification page, Tools & resources and help & FAQS</p>
+                        <p>Google digital garage is basically where you can learn digital skills for free at your own pace, with personalised training designed to help you develop your career, market your business, and more.</p>
                         <br>
                         <u style="text-decoration: underline;">TechStack :- </u>
                         <br>
@@ -46,10 +46,9 @@ projectsSection.innerHTML = `
                         <p>Developed a clone of <a href="https://www.dermstore.com/"
                                 style="color: #FFF; text-decoration: underline;" target="_blank">Dermstore</a> website</p>
                         <br >
-                        <p>its an individual project, build dermstore clone in 6 days</p>
-                        </p>
+                        <p>it is an individual project, build the application in 5 days.</p>
                         <br>
-                        <p>I developed everything in this project what you seen in demo</p>
+                        <p>Dermstore is skin care website for beauty products online. They provide harmless products and we can book in online, They delivered PAN India location.</p>
                         <br>
                         <u style="text-decoration: underline;">TechStack :- </u>
                         <br>
@@ -81,10 +80,10 @@ projectsSection.innerHTML = `
                         <p>Developed a clone of <a href="https://www.tanishq.co.in//"
                                 style="color: #FFF; text-decoration: underline;" target="_blank">Tanishq</a> website</p>
                         <br >
-                        <p>Along with 5 other members, build lybrate clone in 6 days</p>
+                        <p>Along with 5 other members, build this clone in 6 days</p>
                         </p>
                         <br>
-                        <p>I developed Login page both sign-up ans sign-in using</p>
+                        <p>Tanishq is online Gold & Diamond jewellery shopping store. customers can book an appointment for special design. Tanishq store is a very accurate way of measuring the purity of gold.</p>
                         <br>
                         <u style="text-decoration: underline;">TechStack :- </u>
                         <br>
